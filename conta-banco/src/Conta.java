30593e8f34d2dc8e2c20278342769ca3df9c33fe
Luiz Nascimento <luns.wm@hotmail.com>
2023-08-23 22:03:53 -0300

    feat: implementando a classe conta e definindo seu atributos

---
+++ conta-banco/src/Conta.java
@@ -0,0 +1,42 @@
+public class Conta {
+    private int numeroDaConta;
+    private String codigoAgencia;
+    private String nomeCliente;
+    private Double saldoDaConta;
+    
+    public int getNumeroDaConta() {
+        return numeroDaConta;
+    }
+    public void setNumeroDaConta(int numeroDaConta) {
+        this.numeroDaConta = numeroDaConta;
+    }
+    public String getCodigoAgencia() {
+        return codigoAgencia;
+    }
+    public void setCodigoAgencia(String codigoAgencia) {
+        this.codigoAgencia = codigoAgencia;
+    }
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+    public Double getSaldoDaConta() {
+        return saldoDaConta;
+    }
+    public void setSaldoDaConta(Double saldoDaConta) {
+        this.saldoDaConta = saldoDaConta;
+    }
+    
+    
+
+    
+}
+
+/*
+ * Numero	Inteiro	1021
+Agencia	Texto	067-8
+	Texto	MARIO ANDRADE
+Saldo	Decimal	237.48
+ */
--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+        Conta conta = new Conta();
+        System.out.println("\n\nPor favor, digite o número da Agência !");
+
+        System.out.print("Agência: ");
+        conta.setCodigoAgencia(sc.nextLine());
+
+        System.out.print("Usuário: ");
+        conta.setNomeCliente(sc.nextLine());
+
+        System.out.print("Numero:  ");
+        conta.setNumeroDaConta(sc.nextInt());
+
+        System.out.print("Saldo: ");
+        conta.setSaldoDaConta(sc.nextDouble());
+
+        String disponivel = "";
+
+        if (conta.getSaldoDaConta() > 0.0) {
+            disponivel = String.format("seu saldo %.2f já está disponível para saque. ", conta.getSaldoDaConta());
+        } else {
+            disponivel = String.format("seu saldo %.2f você está sem limites para saque. ",
+                    conta.getSaldoDaConta());
+
+        }
+
+        String cadastroCliente = String.format("Olá %s, obrigado por criar uma conta em nosso banco,"
+                + "sua agência é %s, conta %d e ", conta.getNomeCliente(),
+                conta.getCodigoAgencia(), conta.getNumeroDaConta()).concat(disponivel);
+
+        System.out.println("\n"+cadastroCliente+"\n");
+        sc.close();
     }
 }
